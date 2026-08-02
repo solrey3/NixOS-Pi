02c48f745e9d76d681ff0542ccc7ca5c2b48a538
Solito Reyes III <1338405+solrey3@users.noreply.github.com>
2026-08-02 17:55:50 -0400

    Run P2P apps through Proton VPN

--- modules/home/budchris/desktop-apps.nix
+++ modules/home/budchris/desktop-apps.nix
@@ -1,9 +1,29 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
+let
+  # These launchers enter the system-managed VPN-only network namespace.  The
+  # actual applications cannot fall back to the normal network if proton0 goes
+  # away.
+  vpnAppLaunchers = pkgs.writeShellScriptBin "nicotine" ''
+    exec /run/wrappers/bin/sudo -n /run/current-system/sw/bin/vpn-app-launch nicotine "$@"
+  '';
+  transmissionVpnLauncher = pkgs.writeShellScriptBin "transmission-gtk" ''
+    exec /run/wrappers/bin/sudo -n /run/current-system/sw/bin/vpn-app-launch transmission "$@"
+  '';
+in
 {
   home.packages = with pkgs; [
     nextcloud-client
+    nicotine-plus
     obsidian
+    picard
+    quodlibet # Also provides Ex Falso.
     rpi-imager
+    transmission_4-gtk
+
+    # Take precedence over the applications' unconfined executables while
+    # retaining their desktop files, icons, and other resources.
+    (lib.hiPrio vpnAppLaunchers)
+    (lib.hiPrio transmissionVpnLauncher)
   ];
 }
